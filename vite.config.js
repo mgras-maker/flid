e7aa8729c00b448cd579fba852cf7a88ebbede51
GitHub Pages Deployer <deployer@example.com>
2025-05-27 21:22:41 +0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flid/',
-  build: {
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      }
-    }
-  },
+  base: '/flid/', // Add this line
   server: {
     port: 3000,
     open: true
